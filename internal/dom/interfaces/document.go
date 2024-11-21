5942c28f3f65f693b261aaf5996e5606e7f0508f
amaydixit11 <amaydixit11@gmail.com>
2024-11-22 00:35:39 +0530

    defined document

---
+++ internal/dom/interfaces/document.go
@@ -0,0 +1,56 @@
+package dom
+
+type Document interface {
+	GetImplementation() *DOMImplementation
+	GetURL() string
+	GetDocumentURI() string
+	GetCompatMode() string
+	GetCharacterSet() string
+	GetCharset() string
+	GetInputEncoding() string
+	GetContentType() string
+	GetDoctype() *DocumentType
+	GetDocumentElement() *Element
+	GetElementsByTagName(qualifiedName string) *HTMLCollection
+	GetElementsByTagNameNS(namespace string, localName string) *HTMLCollection
+	GetElementsByClassName(className string) *HTMLCollection
+	CreateElement(localName string, options *ElementCreationOptions) *Element
+	CreateElementNS(namespace string, qualifiedName string, options *ElementCreationOptions) *Element
+	// CreateDocumentFragment() *DocumentFragment
+	CreateTextNode(data string) *Text
+	CreateCDATASection(data string) *CDATASection
+	CreateComment(data string) *Comment
+	CreateProcessingInstruction(target string, data string) *ProcessingInstruction
+	ImportNode(node *Node, deep bool) *Node
+	AdoptNode(node *Node) *Node
+	CreateAttribute(localName string) *Attr
+	CreateAttributeNS(namespace string, qualifiedName string) *Attr
+	CreateEvent(interfaceName string) *Event
+	CreateRange() *Range
+	CreateNodeIterator(root *Node, whatToShow uint64, filter *NodeFilter) *NodeIterator
+	CreateTreeWalker(root *Node, whatToShow uint64, filter *NodeFilter) *TreeWalker
+}
+type ElementCreationOptions struct {
+	string is
+}
+
+type DOMImplementation interface {
+	CreateDocumentType(qualifiedName string, publicId string, systemId string) *DocumentType
+	// CreateDocument(namespace string, qualifiedName string, doctype *DocumentType) *XMLDocument
+	CreateHTMLDocument(title string) *Document
+	// HasFeature() *DocumentType // Always True
+}
+type DocumentType interface {
+	GetName() string
+	GetPublicId() string
+	GetSystemId() string
+}
+
+// type DocumentFragment interface {
+// }
+
+type HTMLCollection interface {
+	GetLength() uint64
+	GetTtem(index uint64) *Element
+	GetNamedItem(name string) *Element
+}
---
+++ internal/dom/interfaces/node.go
@@ -0,0 +1,68 @@
+package dom
+
+type NodeType uint16
+
+const (
+	ELEMENT_NODE                NodeType = 1
+	ATTRIBUTE_NODE              NodeType = 2
+	TEXT_NODE                   NodeType = 3
+	CDATA_SECTION_NODE          NodeType = 4
+	ENTITY_REFERENCE_NODE       NodeType = 5
+	ENTITY_NODE                 NodeType = 6
+	PROCESSING_INSTRUCTION_NODE NodeType = 7
+	COMMENT_NODE                NodeType = 8
+	DOCUMENT_NODE               NodeType = 9
+	DOCUMENT_TYPE_NODE          NodeType = 10
+	DOCUMENT_FRAGMENT_NODE      NodeType = 11
+	NOTATION_NODE               NodeType = 12
+)
+
+type DocumentPosition uint16
+
+const (
+	DOCUMENT_POSITION_DISCONNECTED            DocumentPosition = 0x01
+	DOCUMENT_POSITION_PRECEDING               DocumentPosition = 0x02
+	DOCUMENT_POSITION_FOLLOWING               DocumentPosition = 0x04
+	DOCUMENT_POSITION_CONTAINS                DocumentPosition = 0x08
+	DOCUMENT_POSITION_CONTAINED_BY            DocumentPosition = 0x10
+	DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC DocumentPosition = 0x20
+)
+
+// Node interface represents the Node behaviors in DOM.
+type Node interface {
+	GetNodeType() NodeType
+	GetNodeName() string
+	GetBaseURI() string
+	IsConnected() bool
+	GetOwnerDocument() *Document
+	GetRootNode(options *GetRootNodeOptions) *Node
+	GetParentNode() *Node
+	GetParentElement() *Element
+	HasChildNodes() bool
+	GetChildNodes() []Node
+	GetFirstChild() *Node
+	GetLastChild() *Node
+	GetPreviousSibling() *Node
+	GetNextSibling() *Node
+	GetNodeValue() string
+	SetNodeValue(value string)
+	GetTextContent() string
+	SetTextContent(content string)
+	Normalize()
+	CloneNode(deep bool) *Node
+	IsEqualNode(otherNode *Node) bool
+	IsSameNode(otherNode *Node) bool
+	CompareDocumentPosition(otherNode *Node) DocumentPosition
+	Contains(otherNode *Node) bool
+	LookupPrefix(namespace string) string
+	LookupNamespaceURI(prefix string) string
+	IsDefaultNamespace(namespace string) bool
+	InsertBefore(newNode *Node, referenceNode *Node) *Node
+	AppendChild(newNode *Node) *Node
+	ReplaceChild(newNode *Node, oldNode *Node) *Node
+	RemoveChild(childNode *Node) *Node
+}
+
+type GetRootNodeOptions struct {
+	Composed bool
+}
